65d467aa9f741a63b92a0a74c0ea9007984b6208
Soumojit Datta <tikudatta2052@gmail.com>
2025-02-12 00:02:13 +0530

    🎉 Init Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { DrawerDemo } from "./components/Drawer/drawer";
+
+
+function App() {
+  return (
+    <>
+        <DrawerDemo />
+    </>
+  );
+}
+
+export default App;
